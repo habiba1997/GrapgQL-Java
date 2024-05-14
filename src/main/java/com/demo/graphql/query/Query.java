c59e95d440e948537e4935a30cff4cbcfb1f627d
Habiba.Ahmed <habiba.ahmed@integrant.com>
2024-05-14 14:26:49 +0300

    Creating an Object input graphql query and ! for required fields

--- src/main/java/com/demo/graphql/query/Query.java
+++ src/main/java/com/demo/graphql/query/Query.java
@@ -1,5 +1,7 @@
 package com.demo.graphql.query;
 
+import com.demo.graphql.request.SampleRequest;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 
 import org.springframework.stereotype.Controller;
@@ -7,14 +9,20 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class Query {
 
-    @QueryMapping
-    public String firstQuery() {
-        return "First Query";
-    }
+	@QueryMapping
+	public String firstQuery () {
+		return "First Query";
+	}
+
+	@QueryMapping
+	public String secondQuery () {
+		return "Second Query";
+	}
 
     @QueryMapping
-    public String secondQuery() {
-        return "Second Query";
+    public String getFullName(@Argument SampleRequest sampleRequest) {
+        return sampleRequest.getFirstName() + " " + sampleRequest.getLastName();
     }
-
 }
+
+
